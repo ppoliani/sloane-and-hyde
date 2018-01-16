63c4030711f7a393c76c44e00be35b1cd003249f
ppoliani <ppoliani@gmail.com>
2018-01-16 16:49:48 +0000

    fix compiler errors

--- src/solidity/contracts/SLADCoin.sol
+++ src/solidity/contracts/SLADCoin.sol
@@ -9,11 +9,9 @@ import "zeppelin-solidity/contracts/token/DetailedERC20.sol";
  */
 contract SLADCoin is StandardToken, DetailedERC20 {
   uint256 public totalSupply;
-  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
   function SLADCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) public DetailedERC20(_name, _symbol, _decimals) {
     totalSupply = _supply * (10 ** uint256(decimals));
-    balanceOf[msg.sender] = totalSupply;
+    balances[msg.sender] = totalSupply;
   }
-
 }
