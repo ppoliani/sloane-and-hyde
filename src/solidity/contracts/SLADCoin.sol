ac67991adf660f51e7d02785123c22a7792765db
ppoliani <ppoliani@gmail.com>
2018-01-16 17:12:15 +0000

    add ownable

--- src/solidity/contracts/SLADCoin.sol
+++ src/solidity/contracts/SLADCoin.sol
@@ -2,15 +2,16 @@ pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/StandardToken.sol";
 import "zeppelin-solidity/contracts/token/DetailedERC20.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
    @title Token, an extension of ERC20 token standard
    Uses OpenZeppelin StandardToken.
  */
-contract SLADCoin is StandardToken, DetailedERC20 {
+contract SLADCoin is StandardToken, DetailedERC20, Ownable {
   uint256 public totalSupply;
 
-  function SLADCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) public DetailedERC20(_name, _symbol, _decimals) {
+  function SLADCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) public DetailedERC20(_name, _symbol, _decimals) Ownable() {
     totalSupply = _supply * (10 ** uint256(decimals));
     balances[msg.sender] = totalSupply;
   }
