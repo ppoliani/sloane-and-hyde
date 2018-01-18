df291637f22dc7230815877e831be10040d06ce2
ppoliani <ppoliani@gmail.com>
2018-01-18 16:12:46 +0000

    add empty transactio tab

--- src/js/ui/src/view/tabs/index.jsx
+++ src/js/ui/src/view/tabs/index.jsx
@@ -3,6 +3,7 @@ import Tabs, {Tab} from 'material-ui/Tabs'
 import AppBar from 'material-ui/AppBar'
 import BalanceTab from './BalanceTab'
 import WhiteListTab from './WhiteListTab'
+import TransactionTab from './TransactionTab'
 
 class TabContainer extends Component {
   state = {
@@ -26,7 +27,7 @@ class TabContainer extends Component {
         </Tabs>
         {selectedTab === 0 && <BalanceTab token={token} getBalanceOf={getBalanceOf} />}
         {selectedTab === 1 && <WhiteListTab />}
-        {selectedTab === 2 && <div>Transfer</div>}
+        {selectedTab === 2 && <TransactionTab />}
       </AppBar>
     )
   }
