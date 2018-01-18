67064a08ffce424ca90f9ac5d8e69c789cbcb869
ppoliani <ppoliani@gmail.com>
2018-01-18 18:10:38 +0000

    check that whitelist addresses doesn't include duplicate entries

--- src/solidity/contracts/SLADCoin.sol
+++ src/solidity/contracts/SLADCoin.sol
@@ -10,36 +10,44 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
  */
 contract SLADCoin is StandardToken, DetailedERC20, Ownable {
   uint256 public totalSupply;
-  mapping(address => bool) public whitelist;
-  address[] public balanceAddresses;
+  mapping(address => WhitelistData) public whitelist;
+  address[] public whitelistAddreses;
+
+  struct WhitelistData {
+    bool status;
+    uint256 index;
+  }
 
   function SLADCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) public DetailedERC20(_name, _symbol, _decimals) Ownable() {
     totalSupply = _supply; // * (10 ** uint256(decimals));
-    whitelist[msg.sender] = true;
+    whitelist[msg.sender] = WhitelistData(true, 0);
     balances[msg.sender] = totalSupply;
-    balanceAddresses.push(msg.sender);
   }
 
   modifier isWhitelisted(address addr) {
-    assert(whitelist[addr]);
+    assert(whitelist[addr].status);
     _;
   }
 
-  function getBalanceAddresses() public returns(address[]) {
-    return balanceAddresses;
+  function getWhitelistAddresses() public returns(address[]) {
+    return whitelistAddreses;
   }
 
+
   function transfer(address _to, uint256 _value) isWhitelisted(_to) public returns (bool) {
-    assert(super.transfer(_to, _value));
-    
-    if(balanceOf(_to) == 0x0) {
-      balanceAddresses.push(_to);
-    }
+    super.transfer(_to, _value);
   }
 
   //use only one function to add/remove addresses instead of two separated ones.
   function manageWhitelist(address addr, bool status) onlyOwner() returns (bool) { 
-    whitelist[addr] = status;
+    // Insert a new addr to the whitelist
+    if(whitelistAddreses[whitelist[addr].index] != addr) {
+      whitelist[addr] = WhitelistData(status, whitelistAddreses.push(addr) - 1);
+    }
+
+    // update an existing entry
+    whitelist[addr].status = status;
+
     return true;
   }
 
