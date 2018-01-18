ce2411d2e859908518461ac8acff6d1f74f8f49e
valerioleo <mail.valerioleo@gmail.com>
2018-01-18 12:09:36 +0000

    contract has now whitelist management

--- src/solidity/contracts/SLADCoin.sol
+++ src/solidity/contracts/SLADCoin.sol
@@ -10,9 +10,26 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
  */
 contract SLADCoin is StandardToken, DetailedERC20, Ownable {
   uint256 public totalSupply;
+  mapping(address => bool) public whitelist;
+
+  modifier isWhitelisted(address addr) {
+    assert(whitelist[addr]);
+    _;
+  }
+
+  function transfer(address _to, uint256 _value) isWhitelisted(_to) public returns (bool) {
+    super.transfer(_to, _value);
+  }
 
   function SLADCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) public DetailedERC20(_name, _symbol, _decimals) Ownable() {
     totalSupply = _supply * (10 ** uint256(decimals));
     balances[msg.sender] = totalSupply;
   }
+
+  function manageWhitelist(address addr, bool status) returns (bool) {
+    whitelist[addr] = status;
+
+    return true;
+  }
+
 }
--- src/solidity/truffle.js
+++ src/solidity/truffle.js
@@ -4,6 +4,8 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*', // Match any network id
+      gas: 4700036,
+      gasPrice: 22000000000
     }
   }
 };
