06bcb5c0fcb7ff158c18a1842f87d60be7cab7f1
ppoliani <ppoliani@gmail.com>
2018-01-18 16:12:52 +0000

    add empty transactio tab

---
+++ src/js/ui/src/view/tabs/TransactionTab.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import TabContent from './TabContent'
+
+class TransactionTab extends Component {
+  render() {
+    return (
+      <TabContent>Transaction</TabContent>
+    )
+  }
+}
+
+export default TransactionTab
