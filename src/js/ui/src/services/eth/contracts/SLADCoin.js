c41bb3c6d752d095d886606f85a729a7348853fa
ppoliani <ppoliani@gmail.com>
2018-01-19 14:49:25 +0000

    remove comments

--- src/js/ui/src/services/eth/contracts/SLADCoin.js
+++ src/js/ui/src/services/eth/contracts/SLADCoin.js
@@ -2,28 +2,7 @@ import Maybe from 'folktale/maybe'
 import sladCoinContractDefinition from '../../../../../../solidity/build/contracts/SLADCoin.json'
 import getWeb3 from '../'
 
-<<<<<<< HEAD
-const CONTRACT_ADDR = '0xf204a4ef082f5c04bb89f7d5e6568b796096735a'; 
-=======
-const CONTRACT_ADDR = '0x91c1bec5c14aa60863f069bb862aa2fe5c01a1cd';
->>>>>>> 55c66dcf5d84514c1d542085f8aa36453d9b793f
-const contract = getWeb3().eth.contract(sladCoinContractDefinition.abi).at(CONTRACT_ADDR);
-
-// const extendedContract = Object.assign({}, contract, {
-//   // Add a new function becuase currently the way to invkove the fallback function is 
-//   // by utilizing web3.eth.sendTransaction 
-//   sendTransaction: ({from, value}) => { 
-//     const web3 = getWeb3();
-    
-//     return new Promise((resolve, reject) => { 
-//       web3.eth.sendTransaction({from, to: CONTRACT_ADDR, value: web3.toWei(value, 'ether')}, (error, result) => {
-//         if(error) return reject(error);
-//         resolve(result);
-//       })
-//     });
-//   }
-// })
-
-export default contract;
+const CONTRACT_ADDR = '0xaa780861da3d81d3ecef0d2ccfb8ba58466a5de9';
+export default getWeb3().eth.contract(sladCoinContractDefinition.abi).at(CONTRACT_ADDR);
  
 
