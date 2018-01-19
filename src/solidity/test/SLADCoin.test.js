34d9503688c5cbed2b6799ff89dfdd38aef3948a
ppoliani <ppoliani@gmail.com>
2018-01-19 11:41:25 +0000

    use async/await in the tests

--- src/solidity/test/SLADCoin.test.js
+++ src/solidity/test/SLADCoin.test.js
@@ -1,86 +1,66 @@
 const SLADCoin = artifacts.require('SLADCoin');
 
 contract('SLADCoin', accounts => {
-  it('should put SLADCoin in the first account', () => {
-    return SLADCoin.deployed()
-    .then(instance => instance.balanceOf.call(accounts[0]))
-    .then(balance => {
-      assert.equal(balance.valueOf(), 1000000, `1000000 wasn't in the first account`)
-    });
+  it('should put SLADCoin in the first account', async () => {
+    const instance = await SLADCoin.deployed()
+    const balance = await instance.balanceOf.call(accounts[0])
+  
+    assert.equal(balance.valueOf(), 1000000, `1000000 wasn't in the first account`);
   });
 
-  it('should whitelist owner', () => {
-    return SLADCoin.deployed().then(instance => {
-      instance.whitelist.call(accounts[0]).then(res => {
-        assert.equal(res[0], true, 'Owner was not whitelisted')
-      })
-    });
+  it('should whitelist owner', async () => {
+    const instance = await SLADCoin.deployed()
+    const res = await instance.whitelist.call(accounts[0]);
+
+    assert.equal(res[0], true, 'Owner was not whitelisted');
   });
 
-  it('should update whitelist', () => {
-    return SLADCoin.deployed().then(instance => {
-      instance.whitelist.call(accounts[1]).then(res => {
-        assert.equal(res[0], false, 'Account 1 is correctly NOT whitelisted')
-      });
+  it('should update whitelist', async () => {
+    const instance = await SLADCoin.deployed();
+    const res = await instance.whitelist.call(accounts[1]);
+    assert.equal(res[0], false, 'Account 1 is correctly NOT whitelisted');
 
-      instance.manageWhitelist(accounts[1], true).then(() => {
-        instance.whitelist.call(accounts[1]).then(res => {
-          assert.equal(res[0], true, 'Account is correctly whitelisted')
-        });
-      })
-    })
-  })
+    await instance.manageWhitelist(accounts[1], true);
+    const res2 = await instance.whitelist.call(accounts[1]);
 
-  it('should return all whitelisted addresses', () => {
-    return SLADCoin.deployed().then(instance => {
-      instance.getWhitelistAddresses.call().then(res => {
-        assert.deepEqual(res, [accounts[0], accounts[1]], 'Wrong list of whitelisted addresses');
-      });
-    });
+    assert.equal(res2[0], true, 'Account is correctly whitelisted');
+  });
+
+  it('should return all whitelisted addresses', async () => {
+    const instance = await SLADCoin.deployed()
+    const res = await instance.getWhitelistAddresses.call();
+    assert.deepEqual(res, [accounts[0], accounts[1]], 'Wrong list of whitelisted addresses');
   });
 
   it('should not add an addresses to the whitelist addresses if it already exists', async () => {
-    return SLADCoin.deployed().then(async instance => {
-      const whitelistAddresses = await instance.getWhitelistAddresses.call();
-      assert.deepEqual(whitelistAddresses, [accounts[0], accounts[1]], 'Wrong list of whitelisted addresses');
+    const instance = await SLADCoin.deployed();
+    const whitelistAddresses = await instance.getWhitelistAddresses.call();
+    assert.deepEqual(whitelistAddresses, [accounts[0], accounts[1]], 'Wrong list of whitelisted addresses');
 
-      const res = await instance.manageWhitelist(accounts[1], true);
-      const whitelistAddresses2 = await instance.getWhitelistAddresses.call();
-      assert.deepEqual(whitelistAddresses2, [accounts[0], accounts[1]], 'Should not add the same address twice');
-    });
+    const res = await instance.manageWhitelist(accounts[1], true);
+    const whitelistAddresses2 = await instance.getWhitelistAddresses.call();
+    assert.deepEqual(whitelistAddresses2, [accounts[0], accounts[1]], 'Should not add the same address twice');
   });
 
-  it('should send coin correctly to whitelisted address', () => {
-    let meta;
+  it('should send coin correctly to whitelisted address', async () => {
     // Get initial balances of first and second account.
+    let balance;
     const account_one = accounts[0];
     const account_two = accounts[1];
-
-    let account_one_starting_balance;
-    let account_two_starting_balance;
-    let account_one_ending_balance;
-    let account_two_ending_balance;
-
     const amount = 10;
 
-    return SLADCoin.deployed().then(instance => {
-      meta = instance;
-      return meta.balanceOf.call(account_one);
-    }).then((balance) => {
-      account_one_starting_balance = balance.toNumber();
-      return meta.balanceOf.call(account_two);
-    }).then((balance) => {
-      account_two_starting_balance = balance.toNumber();
-      return meta.transfer(account_two, amount, { from: account_one });
-    })
-    .then(() => meta.balanceOf.call(account_one))
-    .then(balance => {
-      account_one_ending_balance = balance.toNumber();
-      return meta.balanceOf.call(account_two);
-    }).then(balance => {
-      account_two_ending_balance = balance.toNumber();
-      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, `Amount wasn't correctly taken from the sender`);
-      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, `Amount wasn't correctly sent to the receiver`);
-    });
+    const instance = await SLADCoin.deployed();
+    balance = await instance.balanceOf.call(account_one);
+    const account_one_starting_balance = balance.toNumber();
+    balance = await instance.balanceOf.call(account_two);
+    const account_two_starting_balance = balance.toNumber();
+    await instance.transfer(account_two, amount, { from: account_one });
+    balance = await instance.balanceOf.call(account_one);
+    const account_one_ending_balance = balance.toNumber();
+    balance = await instance.balanceOf.call(account_two);
+    account_two_ending_balance = balance.toNumber();
+
+    assert.equal(account_one_ending_balance, account_one_starting_balance - amount, `Amount wasn't correctly taken from the sender`);
+    assert.equal(account_two_ending_balance, account_two_starting_balance + amount, `Amount wasn't correctly sent to the receiver`);
   });
 });
