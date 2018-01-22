f29302848b763f1185ccaf6c32ba64c4f19d5d07
ppoliani <ppoliani@gmail.com>
2018-01-22 17:47:04 +0000

    move checksig to a new module

--- src/js/api/auth/authApi.js
+++ src/js/api/auth/authApi.js
@@ -1,23 +1,6 @@
-const {generateToken} = require('./jwt')
-const ethUtil = require('ethereumjs-util')
 const firebase = require('firebase')
 const {HttpError} = require('../core/api')
-
-const checkSig = (sig, account) => {
-  const message = ethUtil.toBuffer(process.env.DATA_TO_SIGN);
-  const msgHash = ethUtil.hashPersonalMessage(message);
-
-  // Get the address of whoever signed this message
-  const signature = ethUtil.toBuffer(sig);
-  const {v, r, s} = ethUtil.fromRpcSig(signature);
-  const publicKey = ethUtil.ecrecover(msgHash, v, r, s);
-  const sender = ethUtil.publicToAddress(publicKey);
-  const addr = ethUtil.bufferToHex(sender);
-
-  return addr === account
-    ? generateToken({account})
-    : null;
-}
+const {checkSig} = require('../helpers/crypto')
 
 const login = async (ctx) => {
   const {sig, account} = ctx.request.body;
