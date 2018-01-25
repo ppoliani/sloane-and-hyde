b92640fc2dce9e977458c94108ebdf218cfbe0cc
ppoliani <ppoliani@gmail.com>
2018-01-25 16:43:23 +0000

    introduce new implementation of the matching algorithm

--- src/js/api/order/mathing.js
+++ src/js/api/order/mathing.js
@@ -1,45 +1,91 @@
 const {List, Map} = require('immutable')
 
+const groupById = orders => orders.groupBy(o => o.get('id'))
+
 // Expects an order list of bid and ask orders.
-// The ordering is based on the price (descending for bid and ascending for ask orders)
-const match = (order, bidOrders, askOrders) => {
-  return order.type === 'ask' 
-    ? matchAskOrder(order, bidOrders)
-    : matchBidOrder(order, askOrders)
+const match = (bidOrders, askOrders) => {
+  // let i = 0;
+  // let result = [];
+  // let completed = false;
+
+  // while(!completed && i < bidOrders.size) {
+  //   const bidOrder = bidOrders.get(i);
+  //   [bidOrder, askOrders] = matchOrders(bidOrder, askOrders);
+
+  //   if(matches.size === 0) {
+  //     completed = true;
+  //   }
+
+  //   result = [...result, ...matches];
+  //   i += 1;
+  // }
+
+  return newMatchOrders(Map(), bidOrders, askOrders);
 }
 
 const matchAskOrder = (order, bidOrders) => {
 }
 
-const fillOrder = (askOrder, qty) => {
-  const orderQty = askOrder.get('qty');
+const fillOrder = (order, qty) => {
+  const orderQty = order.get('qty');
   const filled = qty >= orderQty
     ? orderQty
     : orderQty - qty;
 
-  return askOrder
-    .set('filled', filled)
+  return order
+    .set('filled', filled + order.get('filled', 0))
     .set('qty', orderQty - filled);
 }
 
-const matchBidOrder = (order, askOrders) => {
-  const {qty, price} = order.toJS();
+const matchOrders = (bidOrder, askOrders) => {
+  const {qty, price} = bidOrder.toJS();
 
   return askOrders.reduce((filledOrders, askOrder) => {
     const unFilledQty = filledOrders.get('unFilledQty');
 
     if(unFilledQty > 0 && price >= askOrder.get('price')) {
       const newQty = askOrder.get('qty') - unFilledQty;
-      const updatedAskOrder = fillOrder(askOrder.get('qty'), unFilledQty);
+      const updatedAskOrder = fillOrder(askOrder, unFilledQty);
+      const updatedBidOrder = fillOrder(bidOrder, unFilledQty);
 
       return filledOrders
-        .update(['list'], l => l.push(updatedAskOrder))
+        .update('list', l => l.push([updatedAskOrder, updatedBidOrder]))
         .set('unFilledQty', Math.abs(Math.min(newQty, 0)));
     }
 
     return filledOrders;
   }, Map({ list: List(), unFilledQty: qty }))
-  .get('list');
+  .get('list')
+}
+
+
+/// NEW APPROACH
+const newMatchOrders = (updatedOrders, bidOrders, askOrders) => {
+  let topAsk = askOrders.get(0);
+  let topBid = bidOrders.get(0);
+
+  if(topBid.get('price') < topAsk.get('price')) return updatedOrders;
+
+  // update orders
+  const askQty = topAsk.get('qty');
+  const bidQty = topBid.get('qty');
+  
+  topAsk = fillOrder(topAsk, bidQty);
+  topBid = fillOrder(topBid, askQty);
+
+  updatedOrders = updatedOrders
+    .set(topAsk.get('id'), topAsk)
+    .set(topBid.get('id'), topBid);
+
+  bidOrders = topBid.get('qty') === 0
+    ? bidOrders.shift()
+    : bidOrders.set(0, topBid);
+  
+  askOrders = topAsk.get('qty') === 0
+    ? askOrders.shift()
+    : askOrders.set(0, topAsk);
+
+  return newMatchOrders(updatedOrders, bidOrders, askOrders);
 }
 
 module.exports = {match}
--- src/js/api/order/mathing.test.js
+++ src/js/api/order/mathing.test.js
@@ -4,31 +4,53 @@ const {match} = require('./mathing')
 
 test.beforeEach(t => {
   t.context.bidOrders = fromJS([
-    {qty: 100, price: 20, filled: 0, type: 'bid'},
-    {qty: 100, price: 15, filled: 0, type: 'bid'},
-    {qty: 50, price: 10, filled: 0, type: 'bid'},
+    {id: 1, qty: 100, price: 20, filled: 0, type: 'bid'},
+    {id: 2, qty: 100, price: 15, filled: 0, type: 'bid'},
+    {id: 3, qty: 50, price: 10, filled: 0, type: 'bid'},
   ]);
 
   t.context.askOrders = fromJS([
-    {qty: 100, price: 30, filled: 0, type: 'ask'},
-    {qty: 200, price: 40, filled: 0, type: 'ask'},
-    {qty: 200, price: 50, filled: 0, type: 'ask'}
+    {id: 4, qty: 100, price: 30, filled: 0, type: 'ask'},
+    {id: 5, qty: 200, price: 40, filled: 0, type: 'ask'},
+    {id: 6, qty: 200, price: 50, filled: 0, type: 'ask'}
   ]);
 });
 
-test('match bid order should return empty array if no orders where filled', async t => {
+test('match order should return empty array if no orders where filled', async t => {
   const {bidOrders, askOrders} = t.context;
-  const order = fromJS({qty: 50, price: 5, type: 'bid'});
+  const result = [...match(bidOrders, askOrders).values()];
 
-  t.deepEqual(match(order, bidOrders, askOrders).toJS(), []);
+  t.deepEqual(result, []);
 });
 
-test('match bid orders partially fill the first order', async t => {
-  const {bidOrders, askOrders} = t.context;
-  const order = fromJS({qty: 50, price: 30, type: 'bid'});
+test('match orders should partially fill the first order', async t => {
+  const {askOrders} = t.context;
+  const bidOrders = t.context.bidOrders.unshift(fromJS({id: 7, qty: 50, price: 30, type: 'bid'}));
+  
   const expected = [
-    {qty: 50, price: 30, type: 'bid'}
+    {id: 4, qty: 50, price: 30, filled: 50, type: 'ask'},
+    {id: 7, qty: 0, price: 30, filled: 50, type: 'bid'}
   ];
 
-  t.deepEqual(match(order, bidOrders, askOrders).toJS(), [{}]);
+  const result = [...match(bidOrders, askOrders).values()].map(o => o.toJS());
+  t.deepEqual(result, expected);
+});
+
+test('match orders should fill multiple orders', async t => {
+  const {askOrders} = t.context;
+  const bidOrders = t.context.bidOrders.unshift(
+    fromJS({id: 7, qty: 50, price: 30, type: 'bid'}),
+    fromJS({id: 8, qty: 100, price: 30, type: 'bid'})
+  );
+  
+  const expected = [
+    {id: 4, qty: 0, price: 30, filled: 100, type: 'ask'},
+    {id: 7, qty: 0, price: 30, filled: 50, type: 'bid'},
+    {id: 8, qty: 50, price: 30, filled: 50, type: 'bid'}
+  ];
+
+  debugger;
+  const result = [...match(bidOrders, askOrders).values()].map(o => o.toJS());
+
+  t.deepEqual(result, expected);
 });
