d48e40284fae6c44d365ef57e6fcecaf81a14087
ppoliani <ppoliani@gmail.com>
2018-01-25 17:02:27 +0000

    introduce new implementation of the matching algorithm

--- src/js/api/order/mathing.js
+++ src/js/api/order/mathing.js
@@ -1,30 +1,7 @@
 const {List, Map} = require('immutable')
 
-const groupById = orders => orders.groupBy(o => o.get('id'))
-
 // Expects an order list of bid and ask orders.
-const match = (bidOrders, askOrders) => {
-  // let i = 0;
-  // let result = [];
-  // let completed = false;
-
-  // while(!completed && i < bidOrders.size) {
-  //   const bidOrder = bidOrders.get(i);
-  //   [bidOrder, askOrders] = matchOrders(bidOrder, askOrders);
-
-  //   if(matches.size === 0) {
-  //     completed = true;
-  //   }
-
-  //   result = [...result, ...matches];
-  //   i += 1;
-  // }
-
-  return newMatchOrders(Map(), bidOrders, askOrders);
-}
-
-const matchAskOrder = (order, bidOrders) => {
-}
+const match = (bidOrders, askOrders) => matchOrders(Map(), bidOrders, askOrders);
 
 const fillOrder = (order, qty) => {
   const orderQty = order.get('qty');
@@ -37,30 +14,7 @@ const fillOrder = (order, qty) => {
     .set('qty', orderQty - filled);
 }
 
-const matchOrders = (bidOrder, askOrders) => {
-  const {qty, price} = bidOrder.toJS();
-
-  return askOrders.reduce((filledOrders, askOrder) => {
-    const unFilledQty = filledOrders.get('unFilledQty');
-
-    if(unFilledQty > 0 && price >= askOrder.get('price')) {
-      const newQty = askOrder.get('qty') - unFilledQty;
-      const updatedAskOrder = fillOrder(askOrder, unFilledQty);
-      const updatedBidOrder = fillOrder(bidOrder, unFilledQty);
-
-      return filledOrders
-        .update('list', l => l.push([updatedAskOrder, updatedBidOrder]))
-        .set('unFilledQty', Math.abs(Math.min(newQty, 0)));
-    }
-
-    return filledOrders;
-  }, Map({ list: List(), unFilledQty: qty }))
-  .get('list')
-}
-
-
-/// NEW APPROACH
-const newMatchOrders = (updatedOrders, bidOrders, askOrders) => {
+const matchOrders = (updatedOrders, bidOrders, askOrders) => {
   let topAsk = askOrders.get(0);
   let topBid = bidOrders.get(0);
 
@@ -69,7 +23,7 @@ const newMatchOrders = (updatedOrders, bidOrders, askOrders) => {
   // update orders
   const askQty = topAsk.get('qty');
   const bidQty = topBid.get('qty');
-  
+
   topAsk = fillOrder(topAsk, bidQty);
   topBid = fillOrder(topBid, askQty);
 
@@ -85,7 +39,7 @@ const newMatchOrders = (updatedOrders, bidOrders, askOrders) => {
     ? askOrders.shift()
     : askOrders.set(0, topAsk);
 
-  return newMatchOrders(updatedOrders, bidOrders, askOrders);
+  return matchOrders(updatedOrders, bidOrders, askOrders);
 }
 
 module.exports = {match}
